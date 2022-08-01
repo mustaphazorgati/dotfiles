400cc341decbc215095f7161ee320288689e9b87
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2022-08-01 16:28:30 +0200

    automatically scaling display on autostart

---
+++ .screenlayout/laptop.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output HDMI-0 --off --output DP-3 --scale 0.5x0.5 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP-4 --off
