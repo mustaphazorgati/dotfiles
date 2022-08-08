388a0443cb5de539e0ca97789a997afe057abb10
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2022-08-08 13:37:25 +0200

    moved nitrogen to screenlayout scripts

--- .screenlayout/laptop.sh
+++ .screenlayout/laptop.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output HDMI-0 --off --output DP-3 --scale 0.5x0.5 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP-4 --off
+xrandr \
+  --output DP-0   --off \
+  --output DP-1   --off \
+  --output DP-2   --off \
+  --output HDMI-0 --off \
+  --output DP-3   --scale 0.5x0.5 --primary --mode 3840x2160 --pos 0x0 --rotate normal \
+  --output DP-4   --off
+
+nitrogen --restore
