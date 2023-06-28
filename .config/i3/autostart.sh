52cce8b1380f75d072cc5c0f22e5b32ee4ca61bd
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2023-06-28 10:24:21 +0200

    config adjustments

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-i3-msg workspace $ws1
+i3-msg workspace number 1
 i3-msg append_layout "$HOME/.config/i3/workspace-1.json"
 i3-msg workspace ssh
-urxvt -name ssh-add -e ssh-add && i3-msg workspace $ws1
+urxvt -name ssh-add -e ssh-add && i3-msg workspace number 1
 
 i3-msg [class="Signal"] focus
