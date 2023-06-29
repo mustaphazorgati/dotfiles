5f0dc063ba0498d590d4fb8f711da51cd015617b
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2023-06-29 04:11:29 +0200

    fixed i3 autostart script

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-i3-msg workspace number 1
-i3-msg append_layout "$HOME/.config/i3/workspace-1.json"
-i3-msg workspace ssh
-urxvt -name ssh-add -e ssh-add && i3-msg workspace number 1
+i3-msg "workspace --no-auto-back-and-forth number 1"
+i3-msg "append_layout $HOME/.config/i3/workspace-1.json"
+i3-msg "workspace --no-auto-back-and-forth ssh"
+urxvt -name ssh-add -e ssh-add && i3-msg "workspace --no-auto-back-and-forth number 1"
 
 i3-msg [class="Signal"] focus
