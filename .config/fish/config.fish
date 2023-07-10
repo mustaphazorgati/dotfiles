96cd02b49015d2575199cde4b57340c88b01e874
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2023-07-10 15:43:36 +0200

    added more abbr/aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,19 +5,24 @@ if status is-interactive
   export GPG_TTY=$(tty)
 
   # alias / abbr
-  abbr rm trash
+  abbr  ... 'cd ../..'
+  alias ... 'cd ../..'
+  abbr  .... 'cd ../../..'
+  alias .... 'cd ../../..'
+  abbr  rm trash
   alias rm trash
-  abbr ll 'ls -lah'
+  abbr  ll 'ls -lah'
   alias ll 'ls -lah'
-  abbr open xdg-open
+  abbr  open xdg-open
   alias open xdg-open
-  abbr pbcopy 'xsel --clipboard --input'
+  abbr  pbcopy 'xsel --clipboard --input'
   alias pbcopy 'xsel --clipboard --input'
-  abbr pbpaste 'xsel --clipboard --output'
+  abbr  pbpaste 'xsel --clipboard --output'
   alias pbpaste 'xsel --clipboard --output'
-  abbr config 'git --git-dir=/home/mustapha/.dotfiles --work-tree=/home/mustapha'
-  alias  config 'git --git-dir=/home/mustapha/.dotfiles --work-tree=/home/mustapha'
-  abbr terminal urxvt
+  abbr  config 'git --git-dir=/home/mustapha/.dotfiles --work-tree=/home/mustapha'
+  alias config 'git --git-dir=/home/mustapha/.dotfiles --work-tree=/home/mustapha'
+  abbr  terminal urxvt
   alias terminal urxvt
+
 end
 
