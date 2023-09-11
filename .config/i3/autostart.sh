81d3ef2ba32a7e317b0c71217bb4ecc571d7625c
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2023-09-11 22:20:51 +0200

    added slack and teams to autostart

--- .config/i3/autostart.sh
+++ .config/i3/autostart.sh
@@ -5,4 +5,9 @@ i3-msg "append_layout $HOME/.config/i3/workspace-1.json"
 i3-msg "workspace --no-auto-back-and-forth ssh"
 urxvt -name ssh-add -e ssh-add && i3-msg "workspace --no-auto-back-and-forth number 1"
 
-i3-msg [class="Signal"] focus
+i3-msg "workspace --no-auto-back-and-forth number 2"
+
+sleep 5
+for win in $(wmctrl -l | awk -F' ' '{print $1}'); do
+    wmctrl -i -r $win -b remove,demands_attention
+done
